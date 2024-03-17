ecc8d5ea9f8ee50303a0e0b3949310cb0a807498
WillWSmith <will.w.smith94@gmail.com>
2024-03-17 14:10:26 -0400

    Bugs fixed minus a css bug issue with table

--- src/components/DailyChores.js
+++ src/components/DailyChores.js
@@ -1,25 +1,27 @@
 import React, { useState, useEffect } from 'react';
-import { db } from '../Firebase'
-import { collection, getDocs } from 'firebase/firestore';
-import { doc, updateDoc } from 'firebase/firestore';
+import { db } from '../Firebase';
+import { collection, getDocs, doc, updateDoc } from 'firebase/firestore';
 
 const daysOfWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
 
 const DailyChores = () => {
   const [chores, setChores] = useState([]);
 
+  // Define fetchData outside of useEffect so it can be reused
+  const fetchData = async () => {
+    try {
+      const querySnapshot = await getDocs(collection(db, 'dailyChores'));
+      const choresData = querySnapshot.docs.map(doc => ({
+        ...doc.data(),
+        id: doc.id
+      }));
+      setChores(choresData);
+    } catch (error) {
+      console.error("Error fetching data: ", error);
+    }
+  };
+
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const querySnapshot = await getDocs(collection(db, 'dailyChores'));
-        const choresData = querySnapshot.docs.map(doc => ({ ...doc.data(), id: doc.id }));
-        setChores(choresData);
-      } catch (error) {
-        console.error("Error fetching data: ", error);
-        // Handle the error appropriately in your UI
-      }
-    };
-  
     fetchData();
   }, []);
 
@@ -41,17 +43,16 @@ const DailyChores = () => {
       default:
         break;
     }
-    try {
-      // Update local state
-      setChores(prevChores => prevChores.map(c =>
-        c.id === choreId ? { ...c, days: { ...c.days, [day]: { completedBy: nextStatus } } } : c
-      ));
   
+    try {
       // Update database
       const choreRef = doc(db, 'dailyChores', choreId);
       await updateDoc(choreRef, {
         [`days.${day}.completedBy`]: nextStatus,
       });
+  
+      // After successful update, refetch data to ensure UI is in sync
+      await fetchData();
     } catch (error) {
       console.error("Error updating document: ", error);
       // Handle the error appropriately in your UI
